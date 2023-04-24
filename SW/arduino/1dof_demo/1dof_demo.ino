849fccd546d66ee6665ee5cfce5f3cd9fa417dbb
ZhouLu <lz354000575@gmail.com>
2023-04-24 15:21:45 +0900

    model design_doublewheels update

--- SW/arduino/1dof_demo/1dof_demo.ino
+++ SW/arduino/1dof_demo/1dof_demo.ino
@@ -1,8 +1,6 @@
 #include <Servo.h>
 Servo myservo;
 
-String message = "0";
-
 void setup() {
   // put your setup code here, to run once:
 
@@ -10,33 +8,9 @@ void setup() {
   Serial.setTimeout(20);
    
   myservo.attach(7);
-  myservo.write(90);
+  myservo.write(95);
 }
 
 void loop() {
-  while (Serial.available() > 0)
-  {
-    message = Serial.readString();
-    message.trim();
-  }
-
-  if (message == "1")
-  {
-    //myservo.write(85);
-  }
-  else if (message == "2")
-  {
-    //myservo.write(85);
-  }
-    else if (message == "q")
-  {
-    myservo.write(85);
-  }
-    else if (message == "e")
-  {
-    myservo.write(95);
-  }
-  else {
-    myservo.write(90);
-  }
+  myservo.write(95);
 }
